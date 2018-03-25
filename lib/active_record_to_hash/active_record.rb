b3a61fbbde8bb4afd629fbad2fca9137009f038b
Masamoto Miyata <miyata@sincere-co.com>
2018-03-25 21:48:46 +0900

    Implement single record relaiton.

--- lib/active_record_to_hash/active_record.rb
+++ lib/active_record_to_hash/active_record.rb
@@ -1,4 +1,11 @@
 module ActiveRecordToHash
+  module_function
+  def handle_with_option(record, attr_name, options)
+    value = record.public_send(attr_name)
+    return value.to_hash(options) if value.is_a? ::ActiveRecord::Base
+    value
+  end
+
   module ActiveRecord
     extend ActiveSupport::Concern
 
@@ -8,12 +15,23 @@ module ActiveRecordToHash
 
     module LocalInstanceMethods
       def to_hash(options = {})
-        attributes.each_with_object({}) do |(k, v), hash|
+        hash = attributes.each_with_object({}) do |(k, v), hash|
           key = k.to_sym
           next if options[:except] && options[:except].include?(key)
           next if options[:only] && !options[:only].include?(key)
           hash[key] = v
         end
+
+        options.each do |k, v|
+          next unless k.to_s.start_with?('with_')
+
+          attr_name = k['with_'.length..-1].to_sym
+          hash_key = options[k] == true || options[k][:key].nil? ? attr_name : options[k][:key]
+          options = options[k] == true ? {} : options[k]
+          hash[hash_key] = ActiveRecordToHash.handle_with_option(self, attr_name, options)
+        end
+
+        hash
       end
     end
   end
--- spec/to_hash_spec.rb
+++ spec/to_hash_spec.rb
@@ -4,8 +4,11 @@ describe 'to_hash' do
   let(:shop) { 
     FactoryBot.create(:shop)
   }
+  let(:area) { 
+    FactoryBot.create(:area)
+  }
   context 'No relations' do
-    example 'All attributes' do
+    example 'No option' do
       expect(shop.to_hash).to match({
         id: shop.id,
         name: shop.name,
@@ -36,6 +39,59 @@ describe 'to_hash' do
         id: shop.id,
         name: shop.name
       })
+    end
+  end
+
+  context 'With relations' do
+    context 'Single record' do
+      example 'No option' do
+        hash = area.to_hash(with_wide_area: true)
+        expect(hash[:wide_area]).to match({
+          id: area.wide_area.id,
+          name: area.wide_area.name,
+          created_at: area.wide_area.created_at,
+          updated_at: area.wide_area.updated_at
+        })
+      end
+
+      example 'Except' do
+        hash = area.to_hash(with_wide_area: {except: [:created_at]})
+        expect(hash[:wide_area]).to match({
+          id: area.wide_area.id,
+          name: area.wide_area.name,
+          updated_at: area.wide_area.updated_at
+        })
+
+        hash = area.to_hash(with_wide_area: {except: [:created_at, :updated_at]})
+        expect(hash[:wide_area]).to match({
+          id: area.wide_area.id,
+          name: area.wide_area.name,
+        })
+      end
+
+      example 'Only' do
+        hash = area.to_hash(with_wide_area: {only: [:name]})
+        expect(hash[:wide_area]).to match({
+          name: area.wide_area.name,
+        })
+
+        hash = area.to_hash(with_wide_area: {only: [:id, :name]})
+        expect(hash[:wide_area]).to match({
+          id: area.wide_area.id,
+          name: area.wide_area.name,
+        })
+      end
+
+      example 'Key' do
+        hash = area.to_hash(with_wide_area: {key: :foobar})
+        expect(hash[:wide_area]).to be nil
+        expect(hash[:foobar]).to match({
+          id: area.wide_area.id,
+          name: area.wide_area.name,
+          created_at: area.wide_area.created_at,
+          updated_at: area.wide_area.updated_at
+        })
+      end
     end
   end
 end
