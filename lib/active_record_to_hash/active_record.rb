cf89045201547744027d4b42c46d8b902d26b0b6
Masamoto Miyata <miyata@sincere-co.com>
2018-03-25 22:16:54 +0900

    Fix rubocop.

--- lib/active_record_to_hash/active_record.rb
+++ lib/active_record_to_hash/active_record.rb
@@ -1,7 +1,7 @@
 module ActiveRecordToHash
   module_function
 
-  def handle_with_option(record, attr_name, options)
+  def retrieve_child_attribute(record, attr_name, options)
     value = record.public_send(attr_name)
     return value.to_hash(options) if value.is_a? ::ActiveRecord::Base
     value
@@ -16,20 +16,20 @@ module ActiveRecordToHash
 
     module LocalInstanceMethods
       def to_hash(options = {})
-        hash = attributes.each_with_object({}) do |(k, v), hash|
+        hash = attributes.each_with_object({}) do |(k, v), memo|
           key = k.to_sym
           next if options[:except] && options[:except].include?(key)
           next if options[:only] && !options[:only].include?(key)
-          hash[key] = v
+          memo[key] = v
         end
 
-        options.each do |k, _v|
+        options.each_key do |k|
           next unless k.to_s.start_with?('with_')
 
-          attr_name = k['with_'.length..-1].to_sym
+          attr_name = k[5..-1].to_sym # 5 is 'with_'.length
           hash_key = options[k] == true || options[k][:key].nil? ? attr_name : options[k][:key]
           options = options[k] == true ? {} : options[k]
-          hash[hash_key] = ActiveRecordToHash.handle_with_option(self, attr_name, options)
+          hash[hash_key] = ActiveRecordToHash.retrieve_child_attribute(self, attr_name, options)
         end
 
         hash
