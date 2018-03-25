24bbd746c74f2baf4d6d1f0029c553c4534ae109
Masamoto Miyata <miyata@sincere-co.com>
2018-03-25 20:59:19 +0900

    Implement only and except options.

--- lib/active_record_to_hash/active_record.rb
+++ lib/active_record_to_hash/active_record.rb
@@ -8,7 +8,12 @@ module ActiveRecordToHash
 
     module LocalInstanceMethods
       def to_hash(options = {})
-        attributes
+        attributes.each_with_object({}) do |(k, v), hash|
+          key = k.to_sym
+          next if options[:except] && options[:except].include?(key)
+          next if options[:only] && !options[:only].include?(key)
+          hash[key] = v
+        end
       end
     end
   end
--- spec/to_hash_spec.rb
+++ spec/to_hash_spec.rb
@@ -6,7 +6,36 @@ describe 'to_hash' do
   }
   context 'No relations' do
     example 'All attributes' do
-      expect(shop.to_hash).to match(shop.attributes)
+      expect(shop.to_hash).to match({
+        id: shop.id,
+        name: shop.name,
+        created_at: shop.created_at,
+        updated_at: shop.updated_at
+      })
+    end
+
+    example 'Except' do
+      expect(shop.to_hash(except: [:created_at])).to match({
+        id: shop.id,
+        name: shop.name,
+        updated_at: shop.updated_at
+      })
+
+      expect(shop.to_hash(except: [:updated_at, :created_at])).to match({
+        id: shop.id,
+        name: shop.name
+      })
+    end
+
+    example 'Only' do
+      expect(shop.to_hash(only: [:name])).to match({
+        name: shop.name,
+      })
+
+      expect(shop.to_hash(only: [:name, :id])).to match({
+        id: shop.id,
+        name: shop.name
+      })
     end
   end
 end
